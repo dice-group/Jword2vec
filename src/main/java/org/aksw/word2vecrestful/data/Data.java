169e8f9e90a64e130e24ac8b5c9787a02f400648
renespeck <speckrene@gmail.com>
2015-05-30 12:29:13 +0200

    Downloads google drive file.

--- src/main/java/org/aksw/word2vecrestful/data/Data.java
+++ src/main/java/org/aksw/word2vecrestful/data/Data.java
@@ -3,61 +3,82 @@ package org.aksw.word2vecrestful.data;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.List;
+import java.util.Scanner;
 import java.util.zip.GZIPInputStream;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-// TODO
 public class Data {
 
     public static Logger LOG          = LogManager.getLogger(Data.class);
-    public static String filename     = "data/GoogleNews-vectors-negative300.bin.gz";
+    public static String filename     = "data/GoogleNews-vectors-negative300.bin";
     public static String filename_zip = filename + ".gz";
 
-    /*
-     * TODO
-     * 
-     * confirm needed!!!!
-     */
-    public static String url          = "https://docs.google.com/uc?export=download&confirm=Sr5U&id=0B7XkCwpI5KDYNlNUTTlSS21pQmM";
+    public static String download_url = "https://docs.google.com/uc?export=download&id=0B7XkCwpI5KDYNlNUTTlSS21pQmM";
 
-    public static void main(String[] a) {
-        Data.downloadFile(url);
-    }
+    public static void downloadFile() {
+        try {
+            // connect to get confirm
+            URL authUrl = new URL(download_url);
+            HttpsURLConnection authCon = (HttpsURLConnection) authUrl.openConnection();
+            authCon.connect();
 
-    /*
-     * https://developers.google.com/drive/web/manage-downloads
-     *
-     * Download a file's content.
-     *
-     * @param service
-     *            Drive API service instance.
-     * @param file
-     *            Drive File instance.
-     * @return InputStream containing the file's content if successful,
-     *         {@code null} otherwise.
+            String content;
+            Scanner scanner = new Scanner(authCon.getInputStream());
+            scanner.useDelimiter("\\A");
+            content = scanner.hasNext() ? scanner.next() : "";
+            scanner.close();
 
-    private static InputStream downloadFile(Drive service, File file) {
-        if (file.getDownloadUrl() != null && file.getDownloadUrl().length() > 0) {
-            try {
-                // uses alt=media query parameter to request content
-                return service.files().get(file.getId()).executeMediaAsInputStream();
-            } catch (IOException e) {
-                // An error occurred.
-                e.printStackTrace();
-                return null;
+            // find confirm
+            String confirm = "";
+            if (!content.isEmpty()) {
+                String start = content.substring(content.indexOf("confirm=") + "confirm=".length());
+                confirm = start.substring(0, start.indexOf("&amp;"));
+                LOG.info(confirm);
             }
-        } else {
-            // The file doesn't have any content stored on Drive.
-            return null;
+
+            // get cookies
+            if (!confirm.isEmpty()) {
+
+                StringBuilder cookieHeaders = new StringBuilder();
+                List<String> cookies = authCon.getHeaderFields().get("Set-Cookie");
+                if (cookies != null) {
+                    for (String cookie : cookies) {
+                        if (cookieHeaders.length() > 0) {
+                            cookieHeaders.append("; ");
+                        }
+                        String value = cookie.split(";")[0];
+                        cookieHeaders.append(value);
+                    }
+                }
+
+                // connect with confirm and cookies
+                URL regUrl = new URL(download_url + "&confirm=" + confirm);
+                HttpsURLConnection regCon = (HttpsURLConnection) regUrl.openConnection();
+                regCon.setRequestProperty("Cookie", cookieHeaders.toString());
+                regCon.connect();
+
+                // download file
+                LOG.info("Downloading file (1.5G), please wait");
+                boolean done = downloadFile(regCon.getInputStream());
+                if (!done)
+                    LOG.warn("Could not download " + download_url);
+            }
+        } catch (Exception e) {
+            LOG.error(e.getLocalizedMessage(), e);
         }
+
     }
-    */
+
     public static boolean downloadFile(String url) {
         try {
             return downloadFile(new URL(url));
@@ -67,15 +88,24 @@ public class Data {
         return false;
     }
 
-    public static boolean downloadFile(URL url) {
+    public static boolean downloadFile(InputStream is) {
         try {
-            ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+            ReadableByteChannel rbc = Channels.newChannel(is);
             FileOutputStream fos = new FileOutputStream(filename_zip);
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
 
             fos.close();
             rbc.close();
             return true;
+        } catch (Exception e) {
+            LOG.error(e.getLocalizedMessage(), e);
+        }
+        return false;
+    }
+
+    public static boolean downloadFile(URL url) {
+        try {
+            return downloadFile(url.openStream());
         } catch (Exception e) {
             LOG.error(e.getLocalizedMessage(), e);
         }
