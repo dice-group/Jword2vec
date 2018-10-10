827dde6dc3b398e2d4abaa74533f223ef880340c
nikit91@gmail.com <nikit91@gmail.com>
2018-10-10 11:38:00 +0200

    adding more test cases

--- src/test/java/org/aksw/word2vecrestful/NormalizedInMemModelTest.java
+++ src/test/java/org/aksw/word2vecrestful/NormalizedInMemModelTest.java
@@ -23,78 +23,73 @@ public class NormalizedInMemModelTest {
 	}
 	public static Logger LOG = LogManager.getLogger(NormalizedInMemModelTest.class);
 
-	/*@Test
-	public void testNormalizedModel() {
-		LOG.info("Starting InMemory indexed model test!");
-		final W2VNrmlMemModel memModel = Word2VecFactory.getNormalizedBinModel();
-		LOG.info("Indexed Model instance created");
-		Map<String, String> wordKeyMap = new HashMap<>();
-		wordKeyMap.put("cat", null);
-		wordKeyMap.put("dog", null);
-		wordKeyMap.put("airplane", null);
-		wordKeyMap.put("road", null);
-		
-		long startTime, diff;
-		long totTime = 0;
-		for (String word : wordKeyMap.keySet()) {
-			LOG.info("Sending query for word :" + word);
-			startTime = System.currentTimeMillis();
-			float[] vec = memModel.getWord2VecMap().get(word);
-			Map<String, float[]> closestWord = memModel.getClosestSubEntry(vec, wordKeyMap.get(word));
-			Assert.assertTrue(closestWord.containsKey(word));
-			diff = System.currentTimeMillis() - startTime;
-			totTime += diff;
-			LOG.info("Query time recorded for the word: '" + word + "' and subset: '" + wordKeyMap.get(word) + "' is "
-					+ diff + " milliseconds.");
-		}
+	/*
+	 * @Test public void testNormalizedModel() {
+	 * LOG.info("Starting InMemory indexed model test!"); final W2VNrmlMemModel
+	 * memModel = Word2VecFactory.getNormalizedBinModel();
+	 * LOG.info("Indexed Model instance created"); Map<String, String> wordKeyMap =
+	 * new HashMap<>(); wordKeyMap.put("cat", null); wordKeyMap.put("dog", null);
+	 * wordKeyMap.put("airplane", null); wordKeyMap.put("road", null);
+	 * 
+	 * long startTime, diff; long totTime = 0; for (String word :
+	 * wordKeyMap.keySet()) { LOG.info("Sending query for word :" + word); startTime
+	 * = System.currentTimeMillis(); float[] vec =
+	 * memModel.getWord2VecMap().get(word); Map<String, float[]> closestWord =
+	 * memModel.getClosestSubEntry(vec, wordKeyMap.get(word));
+	 * Assert.assertTrue(closestWord.containsKey(word)); diff =
+	 * System.currentTimeMillis() - startTime; totTime += diff;
+	 * LOG.info("Query time recorded for the word: '" + word + "' and subset: '" +
+	 * wordKeyMap.get(word) + "' is " + diff + " milliseconds."); }
+	 * 
+	 * LOG.info("Average query time: " + (totTime / wordKeyMap.size()) +
+	 * " milliseconds");
+	 * 
+	 * }
+	 */
 
-		LOG.info("Average query time: " + (totTime / wordKeyMap.size()) + " milliseconds");
+	/*
+	 * @Test public void testNbmTime() {
+	 * LOG.info("Starting InMemory indexed model test!"); final
+	 * W2VNrmlMemModelNonIndxd memModel =
+	 * Word2VecFactory.getNormalizedBinNonIndxdModel(); float[][] centroids =
+	 * {TestConst.CENT1, TestConst.CENT2, TestConst.CENT3, TestConst.CENT4,
+	 * TestConst.CENT5};
+	 * 
+	 * long startTime, diff; long totTime = 0; for (int i=0;i<centroids.length;i++)
+	 * { LOG.info("Sending query for Centroid " + (i+1) ); startTime =
+	 * System.currentTimeMillis(); String closestWord = memModel.getClosestSubEntry(
+	 * centroids[i], null); diff = System.currentTimeMillis() - startTime; totTime
+	 * += diff; LOG.info(closestWord); LOG.info("Query time recorded for Centroid "
+	 * + (i+1) + " is " + diff + " milliseconds."); }
+	 * 
+	 * LOG.info("Average query time: " + (totTime / centroids.length) +
+	 * " milliseconds");
+	 * 
+	 * }
+	 */
 
-	}*/
-	
-	/*@Test
-	public void testNbmTime() {
-		LOG.info("Starting InMemory indexed model test!");
-		final W2VNrmlMemModelNonIndxd memModel = Word2VecFactory.getNormalizedBinNonIndxdModel();
-		float[][] centroids = {TestConst.CENT1, TestConst.CENT2, TestConst.CENT3, TestConst.CENT4, TestConst.CENT5};
-		
-		long startTime, diff;
-		long totTime = 0;
-		for (int i=0;i<centroids.length;i++) {
-			LOG.info("Sending query for Centroid " + (i+1) );
-			startTime = System.currentTimeMillis();
-			String closestWord = memModel.getClosestSubEntry( centroids[i], null);
-			diff = System.currentTimeMillis() - startTime;
-			totTime += diff;
-			LOG.info(closestWord);
-			LOG.info("Query time recorded for Centroid " + (i+1) + " is "
-					+ diff + " milliseconds.");
-		}
-
-		LOG.info("Average query time: " + (totTime / centroids.length) + " milliseconds");
-
-	}*/
-	
 	@Test
 	public void testNbmTime() {
 		LOG.info("Starting InMemory indexed model test!");
 		Word2VecModel nbm = Word2VecFactory.getNormalBinModel();
-		float[][] centroids = { TestConst.CENT1, TestConst.CENT2, TestConst.CENT3, TestConst.CENT4, TestConst.CENT5 };
+		float[][] centroids = { TestConst.CENT1, TestConst.CENT2, TestConst.CENT3, TestConst.CENT4, TestConst.CENT5,
+				TestConst.CENT6, TestConst.CENT7, TestConst.CENT8, TestConst.CENT9, TestConst.CENT10, TestConst.CENT11,
+				TestConst.CENT12, TestConst.CENT13, TestConst.CENT14, TestConst.CENT15, TestConst.CENT16,
+				TestConst.CENT17, TestConst.CENT18, TestConst.CENT19, TestConst.CENT20 };
 		List<String> correctWords = NrmlzdMdlPrfmncTester.getCorrectWords(centroids, nbm);
 		LOG.info("Correct Words are :" + correctWords);
-		
+
 		long startTime, diff;
 		long totTime = 0;
 		List<String> wordSet = new ArrayList<>();
-		for (int i=0;i<centroids.length;i++) {
-			LOG.info("Sending query for Centroid " + (i+1) );
+		for (int i = 0; i < centroids.length; i++) {
+			LOG.info("Sending query for Centroid " + (i + 1));
 			startTime = System.currentTimeMillis();
-			Map<String, float[]> closestWordMap = nbm.getClosestEntry( centroids[i]);
+			Map<String, float[]> closestWordMap = nbm.getClosestEntry(centroids[i]);
 			diff = System.currentTimeMillis() - startTime;
 			totTime += diff;
 			wordSet.addAll(closestWordMap.keySet());
-			LOG.info("Query time recorded for Centroid " + (i+1) + " is "
-					+ diff + " milliseconds.");
+			LOG.info("Query time recorded for Centroid " + (i + 1) + " is " + diff + " milliseconds.");
 		}
 
 		LOG.info("Average query time: " + (totTime / centroids.length) + " milliseconds");
